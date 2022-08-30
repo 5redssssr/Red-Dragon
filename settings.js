12b975ddfd5ead76e32011bc7699b9e8635caecd
5redssssr <112356450+5redssssr@users.noreply.github.com>
2022-08-30 09:56:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,7 +29,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+94785946099'] // owner number
+global.vcardowner = ['+94762814244'] // owner number
 global.ownername = "😎MAHAZONE❯❯" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
@@ -47,8 +47,8 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true]
 
 
-global.owner = ['+94785946099']
-global.ownertag = '+94785946099' //your tag number
+global.owner = ['+94762814244']
+global.ownertag = '+94762814244' //your tag number
 global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
 global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
